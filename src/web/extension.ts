276612883090183f73598074e29930bc2dc8da0d
Ahad Birang <farnabaz@gmail.com>
2022-09-14 09:20:14 +0200

    feat: enable web extension

---
+++ src/web/extension.ts
@@ -0,0 +1,9 @@
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
